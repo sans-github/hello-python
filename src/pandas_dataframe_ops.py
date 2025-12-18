2b827a1218b891babb66133e7ce04b50f607831c
San <San>
2025-12-18 11:47:50 -0800

    Arraanged operations in groups

--- src/pandas_dataframe_ops.py
+++ src/pandas_dataframe_ops.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import pandas as pd
 
 
-def hello_panda():
+def create_df():
     df = pd.DataFrame(
         {
             "num_legs": [2, 4, 8, 0],
@@ -16,6 +16,20 @@ def hello_panda():
     print(df["num_legs"].sample(3, random_state=0))
     print(df["num_legs"].sample(3))
 
+    nums=[1, 2, 3, 4]
+    char=['A', 'B', 'C', 'D']
+
+    df = pd.DataFrame(
+        {
+            "nums": nums, 
+            "char" : char
+        }
+    )
+
+    print(df)
+
+    
+
 
 def hello_panda_csv():
     df_raw = _load_csv_data()
@@ -24,36 +38,28 @@ def hello_panda_csv():
     print(df_raw.sample(3))
     print("{}{}".format("\n\n\n", "===▼▼▼▼▼==="))
 
+    # Column operations
     # df=df_raw.columns # Show columns
     # df=df_raw.info()# Column description, memory usage
 
+    # Slicing operations on column. [start:stop:step]
+    # df=df_raw.columns[:2]   # First 2 columns.
+    # df=df_raw.columns[2:5]  # Columns at index 2, 3, 4
+    # df=df=df_raw.columns[-2:]  # Last 2 columns
+    # df=df_raw.columns[::2]  # Every other column
+    # df=df_raw.columns[7:2:-1] # Start at 7, going back to 2
+
     # df=df_raw.describe()# Count, mean, median, percentile
 
-    # df=df_raw['device'] # Column selection or column indexing
-    # df=df_raw['device'].describe() # Describe selected column
+    # Column selection
+    # df=df_raw['device'] # As series
+    # df=df_raw[['device']] # As DF
+    # df=df_raw[['device', 'country']] # Multiple columns, as DF
 
-    # with pd.option_context('display.max_rows', 1):
-    #     print(df_raw) # Will print only 1 raw inside the context
-
-    # print(df_raw) # Context no longer valid
-
-    # Slicing operations on rows. [start:stop:step]
-    # df_raw[:2]   # First 2 rows.
-    # df_raw[2:5]  # Rows 2, 3, 4
-
-    # Slicing operations on column. [start:stop:step]
-    # df_raw.columns[:2]   # First 2 columns.
-    # df_raw.columns[2:5]  # Columns at index 2, 3, 4
-    # df=df_raw.columns[-2:]  # Last 2 columns
-    # df_raw.columns[::2]  # Every other column
-    # df_raw.columns[7:2:-1] # Start at 7, going back to 2
-
+    # Rows selection. [start:stop:step]
+    # df=df_raw[:2]   # First 2 rows.
+    # df=df_raw[2:5]  # Rows 2, 3, 4
     # df=df_raw.select_dtypes("object")
-
-    # Slicing operations (on data frame). [start:stop:step]
-    # df=df_raw['country'] # Column selection, as a series
-    # df=df_raw[['country']] # Column selection, as DF
-    # df=df_raw[['country', 'user_id']] # To get dataframe, you must use double square brackets
 
     # Position-based indexing, not slicing.
     # df = df_raw.iloc[:, [0, 3]] # Select all rows, and columns 0 and 3
@@ -62,7 +68,7 @@ def hello_panda_csv():
     # df = df_raw.iloc[2:5, :3] # Select rows 2-5, and first 3 column
     # df = df_raw.iloc[2:5, [3]] # Select rows 2-5, and 4rd column
 
-    # Row index is the label for each row in a DataFrame, shown on the far left. By default, pandas uses integers (0, 1, 2, 3...) as row indices.
+    # Note: Row index is the label for each row in a DataFrame, shown on the far left. By default, pandas uses integers (0, 1, 2, 3...) as row indices.
 
     # Label-based indexing
     # df = df_raw.loc[:,'is_fraud'] # all rows, Only is_fraud column
@@ -77,13 +83,11 @@ def hello_panda_csv():
 
     # df=df_raw[df_raw['is_fraud']==1].groupby('country').size() # Fraud by country
 
-    # Pivot by country, show column percent
-    # df=_fraud_by_country(df_raw)
+    # with pd.option_context('display.max_rows', 1):
+    #     print(df_raw) # Will print only 1 raw inside the context
+    # print(df_raw) # Context no longer valid
 
-    # Pivot by country, device, show column percent
-    # df=_fraud_by_country_device(df_raw)
-
-    df = _fraud_by_user(df_raw)
+    df = df_raw
 
     print(df)
 
@@ -165,7 +169,8 @@ def _fraud_by_country_user():
     fraud_country_user_count = (
         df_raw.loc[df_raw["is_fraud"] == 1, ["country", "user_id"]]
         .groupby(["country", "user_id"])
-        .size().sort_values(ascending=False)
+        .size()
+        .sort_values(ascending=False)
     )
 
     fraud_country_user_percent = (
@@ -177,10 +182,15 @@ def _fraud_by_country_user():
     )
 
     # Display everything. Not just a subset
-    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+    with pd.option_context("display.max_rows", None, "display.max_columns", None):
         print(df)
 
+
 if __name__ == "__main__":
-    # hello_panda()
-    # hello_panda_csv()
-    _fraud_by_country_user()
+    # create_df()
+    hello_panda_csv()
+    # _fraud_by_country_user()
+    # df=_fraud_by_country(df_raw)
+    # df=_fraud_by_country_device(df_raw)
+    # df = _fraud_by_user(df_raw)
+    pass
